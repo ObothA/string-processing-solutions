52eadcc4ce4fb3f641e333d98a250de1bf781581
obotha <khyper234@gmail.com>
2021-09-17 20:48:33 +0300

    Modify console log.

--- set2.js
+++ set2.js
@@ -64,37 +64,37 @@ const getHighestbidder = (bidLogs) => {
 
 const bidLogsA = '1,A,5,B,10,A,8,A,17,B,17';
 const solutionA = getHighestbidder(bidLogsA);
-console.log('solutionA:', solutionA);
+console.log(solutionA);
 console.log();
 
 const bidLogsB = '1,Henry,15,Mukasa,24,Barbra,30,Asiimwe,31,Mark,49,Tim,57,Frank,59,Hellen,61,Atim,64,Mukasa,74,Mukasa,69,Mukasa,71,Hellen,78,Frank,78,Barbra,95,Frank,103,Asiimwe,135';
 const solutionB = getHighestbidder(bidLogsB);
-console.log('solutionB:', solutionB);
+console.log(solutionB);
 console.log();
 
 const bidLogsC = '1,Mathius,5,Ruth,10,Carl,19,Mathius,14,Mathius,23,Carl,24,Carl,25,Ruth,26';
 const solutionC = getHighestbidder(bidLogsC);
-console.log('solutionC:', solutionC);
+console.log(solutionC);
 console.log();
 
 const bidLogsD = '1,Lutaya,5,Otim,10,Sheila,19,Lutaya,23,Sheila,24,Lutaya,29,Otim,26';
 const solutionD = getHighestbidder(bidLogsD);
-console.log('solutionD:', solutionD);
+console.log(solutionD);
 console.log();
 
 const bidLogsE = '15';
 const solutionE = getHighestbidder(bidLogsE);
-console.log('solutionE:', solutionE);
+console.log(solutionE);
 console.log();
 
 
 // Responses.
-// solutionA: A,17
+// A,17
 
-// solutionB: Asiimwe,135
+// Asiimwe,135
 
-// solutionC: Ruth,26
+// Ruth,26
 
-// solutionD: Lutaya,29
+// Lutaya,29
 
-// solutionE: ''
+// ''
